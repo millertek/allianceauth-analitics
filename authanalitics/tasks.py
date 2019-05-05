e68a1752b13fc16fdc6a26d5c662bf85f8af8af6
Aaron Kable <aaronkable@gmail.com>
2019-05-05 10:41:41 -0400

    dont error on missing character

--- authanalitics/tasks.py
+++ authanalitics/tasks.py
@@ -99,38 +99,41 @@ def output_stats():
         members = cs.mains
         for member in members:
             date_now = datetime.datetime.now()
-            character = AACharacter.objects.get(character__character_id=member.character.character_id)
+            try:
+                character = AACharacter.objects.get(character__character_id=member.character.character_id)
 
-            qs1 = AAzKillMonth.objects.filter(year=date_now.year-1, month__gte=date_now.month, char=character)
-            qs2 = AAzKillMonth.objects.filter(year=date_now.year, char=character)
-            qs = qs1 | qs2
-            qs_12m = qs.order_by('-year', '-month').aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
-            qs_6m = qs.order_by('-year', '-month')[:6].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
-            qs_3m = qs.order_by('-year', '-month')[:3].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
+                qs1 = AAzKillMonth.objects.filter(year=date_now.year-1, month__gte=date_now.month, char=character)
+                qs2 = AAzKillMonth.objects.filter(year=date_now.year, char=character)
+                qs = qs1 | qs2
+                qs_12m = qs.order_by('-year', '-month').aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
+                qs_6m = qs.order_by('-year', '-month')[:6].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
+                qs_3m = qs.order_by('-year', '-month')[:3].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))['ship_destroyed_sum']
 
-            alts = [co.character for co in character.character.character_ownership.user.character_ownerships.all()]
-            for alt in alts:
-                try:
-                    aa_alt = AACharacter.objects.get(character=alt)
-                    if not aa_alt == character:
-                        qsa1 = AAzKillMonth.objects.filter(year=date_now.year - 1, month__gte=date_now.month, char=aa_alt)
-                        qsa2 = AAzKillMonth.objects.filter(year=date_now.year, char=aa_alt)
-                        qsa = qsa1 | qsa2
-                        qs_12m += qsa.order_by('-year', '-month').aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
-                            'ship_destroyed_sum']
-                        qs_6m += qsa.order_by('-year', '-month')[:6].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
-                            'ship_destroyed_sum']
-                        qs_3m += qsa.order_by('-year', '-month')[:3].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
-                            'ship_destroyed_sum']
-                except:
-                    pass
-                    
-            out_str=[]
-            out_str.append(member.character.character_name)
-            out_str.append(member.character.corporation_name)
-            out_str.append(str(qs_12m))
-            out_str.append(str(qs_6m))
-            out_str.append(str(qs_3m))
-            out_arr.append(out_str)
+                alts = [co.character for co in character.character.character_ownership.user.character_ownerships.all()]
+                for alt in alts:
+                    try:
+                        aa_alt = AACharacter.objects.get(character=alt)
+                        if not aa_alt == character:
+                            qsa1 = AAzKillMonth.objects.filter(year=date_now.year - 1, month__gte=date_now.month, char=aa_alt)
+                            qsa2 = AAzKillMonth.objects.filter(year=date_now.year, char=aa_alt)
+                            qsa = qsa1 | qsa2
+                            qs_12m += qsa.order_by('-year', '-month').aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
+                                'ship_destroyed_sum']
+                            qs_6m += qsa.order_by('-year', '-month')[:6].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
+                                'ship_destroyed_sum']
+                            qs_3m += qsa.order_by('-year', '-month')[:3].aggregate(ship_destroyed_sum=Sum('ships_destroyed'))[
+                                'ship_destroyed_sum']
+                    except:
+                        pass
+                        
+                out_str=[]
+                out_str.append(member.character.character_name)
+                out_str.append(member.character.corporation_name)
+                out_str.append(str(qs_12m))
+                out_str.append(str(qs_6m))
+                out_str.append(str(qs_3m))
+                out_arr.append(out_str)
+            except:
+                pass
             
     return out_arr
