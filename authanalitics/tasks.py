ade017dee707996f47b9503d099817841e71de4a
AaronKable <aaronkable@gmail.com>
2020-04-25 22:43:27 +0800

    refactor

--- authanalitics/tasks.py
+++ authanalitics/tasks.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 import csv
 
-from celery import shared_task
+from celery import shared_task, chain
 
 from .models import AACharacter, AAzKillMonth
 from allianceauth.corputils.models import CorpStats, CorpMember
@@ -16,11 +16,11 @@ from django.utils import timezone
 from dateutil.relativedelta import relativedelta
 from time import sleep
 from math import floor
+from allianceauth.services.tasks import QueueOnce
 
 
 logger = logging.getLogger(__name__)
 
-@shared_task
 def update_character_stats(character_id):
     #logger.info('update_character_stats for %s starting' % str(character_id))
     # https://zkillboard.com/api/stats/characterID/####/
@@ -29,33 +29,28 @@ def update_character_stats(character_id):
     sleep(1)
 
     # https://zkillboard.com/api/characterID/####/kills/
-    _kills_request = requests.get("https://zkillboard.com/api/characterID/" + str(character_id) + "/kills/")
-    _kills_json = _kills_request.json()
-    sleep(1)
+    #_kills_request = requests.get("https://zkillboard.com/api/characterID/" + str(character_id) + "/kills/")
+    #_kills_json = _kills_request.json()
+    #sleep(1)
 
     _last_kill_date = None
-    if len(_kills_json) > 0:
+    #if len(_kills_json) > 0:
         # https://esi.evetech.net/latest/killmails/ID####/HASH####/?datasource=tranquility
-        try:
-            _last_kill_request = requests.get(
-                "https://esi.evetech.net/latest/killmails/" + str(_kills_json[0]['killmail_id']) + "/" +
-                str(_kills_json[0]['zkb']['hash']) + "/?datasource=tranquility")
-            _last_kill_json = _last_kill_request.json()
-            sleep(1)
-            _last_kill_date = parse_datetime(_last_kill_json['killmail_time'])
-        except:
-            pass
+    #    try:
+    #        _last_kill_request = requests.get(
+    #            "https://esi.evetech.net/latest/killmails/" + str(_kills_json[0]['killmail_id']) + "/" +
+    #            str(_kills_json[0]['zkb']['hash']) + "/?datasource=tranquility")
+    #        _last_kill_json = _last_kill_request.json()
+    #        sleep(1)
+    #        _last_kill_date = parse_datetime(_last_kill_json['killmail_time'])
+    #    except:
+    #        pass
 
-    char_model, created = AACharacter.objects.update_or_create(character = EveCharacter.objects.get(character_id=int(character_id)))
-    if created:
-        pass
-    
+    char_model, created = AACharacter.objects.get_or_create(character = EveCharacter.objects.get(character_id=int(character_id)))
 
     if len(_stats_json.get('months', [])) > 0:
         for key, month in _stats_json.get('months', []).items():
             zkill_month, created = AAzKillMonth.objects.get_or_create(char=char_model, year=month.get('year', 0), month=month.get('month', 0))
-            if created:
-                pass
             
             zkill_month.ships_destroyed = month.get('shipsDestroyed', 0)
             zkill_month.ships_lost = month.get('shipsLost', 0)
@@ -78,15 +73,49 @@ def update_character_stats(character_id):
     char_model.save()
 
     #logger.info('update_character_stats for %s complete' % str(character_id))
-def update_char(char_name, char_id):
+
+@shared_task(bind=True, base=QueueOnce)
+def update_char(self, char_id):
     try:
-        logger.info('update_character_stats for %s starting' % str(char_name))
-        update_character_stats(char_id)
-    except:
-        logger.error('update_character_stats failed for %s' % str(char_name))
-        logging.exception("Messsage")
-        sleep(1)  # had an error printed it and skipped it YOLO. better wait a sec to not overload the api
-        pass
+        now = datetime.datetime.now()
+        skip_time =  timezone.now() - relativedelta(hours=24)
+        dt12 = now - relativedelta(months=12)
+        dt6 = now - relativedelta(months=6)
+        dt3 = now - relativedelta(months=3)
+        try:
+            logger.info('update_character_stats for %s starting' % str(char_id))
+            update_character_stats(char_id)
+        except:
+            logger.error('update_character_stats failed for %s' % str(char_id))
+            logging.exception("Messsage")
+            sleep(1)  # had an error printed it and skipped it YOLO. better wait a sec to not overload the api
+        
+        try:
+            character = AACharacter.objects.get(character__character_id=char_id)
+            qs = AAzKillMonth.objects.filter(char=character)
+            qs_12m = qs.filter(year=dt12.year, month__gte=dt12.month) | \
+                        qs.filter(year=now.year)
+            qs_12m = qs_12m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
+            qs_6m = qs.filter(year=dt6.year, month__gte=dt6.month)
+            if now.month <= 6:
+                qs_6m = qs_6m | qs.filter(year=now.year)
+            qs_6m = qs_6m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
+            qs_3m = qs.filter(year__gte=dt3.year, month__gte=dt3.month)
+            if now.month <= 3:
+                qs_3m = qs_3m | qs.filter(year=now.year)
+            qs_3m = qs_3m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
+            character.zk_12m = qs_12m
+            character.zk_6m = qs_6m
+            character.zk_3m = qs_3m
+            character.save()
+        except ObjectDoesNotExist:
+            pass # broken
+
+    except Exception as e:
+        logger.error("failed to update {}: {}".format(char_id, e))
+        return 0  # fail
+    
+    return 1  # pass
 
 
 @shared_task(name='authanaliticis.tasks.run_stat_model_update')
@@ -95,46 +124,16 @@ def run_stat_model_update():
     #logger.info('start')
     active_corp_stats = CorpStats.objects.all()
     member_alliances = ['499005583', '1900696668'] # hardcoded cause *YOLO*
-    stale_date = datetime.datetime.utcnow().replace(tzinfo=timezone.utc) - datetime.timedelta(hours=168)
-    now = datetime.datetime.now()
-    dt12 = now - relativedelta(months=12)
-    dt6 = now - relativedelta(months=6)
-    dt3 = now - relativedelta(months=3)
-	
+
+    sig_list = []
     for cs in active_corp_stats:
         members = cs.mains
         for member in members:
             for alt in member.alts:
                 if alt.alliance_id in member_alliances:
-                    try:
-                        if AACharacter.objects.get(character__character_id=alt.character_id).last_update<stale_date:
-                            update_char(alt.character_name, alt.character_id)
-                    except ObjectDoesNotExist:
-                        update_char(alt.character_name, alt.character_id)
-                    try:
-                        character = AACharacter.objects.get(character__character_id=alt.character_id)
-                        qs = AAzKillMonth.objects.filter(char=character)
-                        qs_12m = qs.filter(year=dt12.year, month__gte=dt12.month) | \
-                                 qs.filter(year=now.year)
-                        qs_12m = qs_12m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
-                        qs_6m = qs.filter(year=dt6.year, month__gte=dt6.month)
-                        if now.month <= 6:
-                            qs_6m = qs_6m | qs.filter(year=now.year)
-                        qs_6m = qs_6m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
-                        qs_3m = qs.filter(year__gte=dt3.year, month__gte=dt3.month)
-                        if now.month <= 3:
-                            qs_3m = qs_3m | qs.filter(year=now.year)
-                        qs_3m = qs_3m.aggregate(ship_destroyed_sum=Coalesce(Sum('ships_destroyed'), 0)).get('ship_destroyed_sum', 0)
-                        character.zk_12m = qs_12m
-                        character.zk_6m = qs_6m
-                        character.zk_3m = qs_3m
-                        character.save()
-                    except ObjectDoesNotExist:
-                        pass # who knows
+                    sig_list.append(update_char.si(alt.character_id))
 
-        #missing = cs.unregistered_members
-        #for member in missing:
-            #update_character_stats.delay(member.character_id)
+    chain(sig_list).apply_async(priority=8)
             
 @shared_task(name='authanaliticis.tasks.run_aggregate_update')
 def run_aggregate_update():
